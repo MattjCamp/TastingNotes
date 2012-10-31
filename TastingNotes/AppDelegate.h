997b28cd83170a99a6a25617421d4a30805e2932
Matthew Campbell <matt@mobileappmastery.com>
2012-10-31 12:03:27 -0400

    Initial Commit

---
+++ TastingNotes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TastingNotes
+//
+//  Created by Matthew Campbell on 10/31/12.
+//  Copyright (c) 2012 Mobile App Mastery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TastingNotes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TastingNotes
+//
+//  Created by Matthew Campbell on 10/31/12.
+//  Copyright (c) 2012 Mobile App Mastery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
