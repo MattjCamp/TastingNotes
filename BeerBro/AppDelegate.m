4f1ad3b593c2f57de21ec1366e364112de760ae0
Matthew Campbell <matt@mobileappmastery.com>
2012-11-07 11:23:19 -0500

    Finally go BeerBro into this project

---
+++ BeerBro/AppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.m
+//  BeerBro
+//
+//  Created by Matthew Campbell on 11/7/12.
+//  Copyright (c) 2012 Mobile App Mastery. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@end
