0c22791256b5a269479da645512585ed608b5f20
Matthew Campbell <matt@mobileappmastery.com>
2012-11-15 13:28:53 -0500

    Added icons and stuff

---
+++ CommanderData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CommanderData
+//
+//  Created by Matthew Campbell on 11/15/12.
+//  Copyright (c) 2012 Mobile App Mastery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
