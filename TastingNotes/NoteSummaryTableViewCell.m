72182e1a186d619ae638bcc72cc03bb2fc120b54
MattjCamp <matt@mobileappmastery.com>
2014-04-22 11:14:37 -0400

    Removed an unnecessary dispatch call

--- TastingNotes/NoteSummaryTableViewCell.m
+++ TastingNotes/NoteSummaryTableViewCell.m
@@ -22,21 +22,20 @@
     self.titleLabel.text = nil;
     self.detailLabel.text = nil;
     
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSDictionary *titleAttributes = @{ NSFontAttributeName : [UIFont boldSystemFontOfSize:14.0f]};
-        NSDictionary *detailAttributes = @{ NSFontAttributeName : [UIFont systemFontOfSize:12.0f]};
-        if(self.note.titleText)
-            self.titleLabel.attributedText = [[NSMutableAttributedString alloc] initWithString:self.note.titleText
-                                                                                    attributes:titleAttributes];
-        if(self.note.detailText)
-            self.detailLabel.attributedText = [[NSMutableAttributedString alloc] initWithString:self.note.detailText
-                                                                                     attributes:detailAttributes];
-        [UIView animateWithDuration:1.00 animations:^{
-            self.noteThumbnail.alpha = 0;
-            self.noteThumbnail.image = self.note.thumbnail;
-            self.noteThumbnail.alpha = 1;
-        }];
-    });
+    
+    NSDictionary *titleAttributes = @{ NSFontAttributeName : [UIFont boldSystemFontOfSize:14.0f]};
+    NSDictionary *detailAttributes = @{ NSFontAttributeName : [UIFont systemFontOfSize:12.0f]};
+    if(self.note.titleText)
+        self.titleLabel.attributedText = [[NSMutableAttributedString alloc] initWithString:self.note.titleText
+                                                                                attributes:titleAttributes];
+    if(self.note.detailText)
+        self.detailLabel.attributedText = [[NSMutableAttributedString alloc] initWithString:self.note.detailText
+                                                                                 attributes:detailAttributes];
+    [UIView animateWithDuration:1.00 animations:^{
+        self.noteThumbnail.alpha = 0;
+        self.noteThumbnail.image = self.note.thumbnail;
+        self.noteThumbnail.alpha = 1;
+    }];
 }
 
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated{
